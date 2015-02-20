217b818192d40b6039b5dd3fadbe8b8b9d298949
Guille <ggalmazor@gmail.com>
2015-02-20 12:08:13 +0100

    Actualizadas dependencias del curso

--- src/server.js
+++ src/server.js
@@ -1,5 +1,5 @@
 var server = require('express')(),
-    Faker = require('Faker'),
+    faker = require('faker'),
     moment = require('moment'),
     cors = require('cors'),
     uuid = require('node-uuid');
@@ -10,15 +10,15 @@ var comics = [];
 while (comics.length < 50) {
   comics.push({
     uuid: uuid.v4(),
-    name: Faker.Company.companyName()
+    title: faker.company.companyName()
   });
 }
 
 server.use(cors());
 
 server.get('/v1/comics', function (req, res, next) {
-  var count = parseInt(req.param('count'), 10) || 10,
-      page = parseInt(req.param('page'), 10) || 0,
+  var count = parseInt(req.params.count, 10) || 10,
+      page = parseInt(req.params.page, 10) || 0,
       from = page * count,
       to = (page + 1) * count;
   var items = comics.slice(from, to);
