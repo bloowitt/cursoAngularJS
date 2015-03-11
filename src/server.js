b7269808d07b15b83b0e7ebcc29808aa0230a90c
Guille <ggalmazor@gmail.com>
2015-03-11 11:40:45 +0100

    Corregido server.js

--- src/server.js
+++ src/server.js
@@ -17,10 +17,11 @@ while (comics.length < 50) {
 server.use(cors());
 
 server.get('/v1/comics', function (req, res, next) {
-  var count = parseInt(req.params.count, 10) || 10,
-      page = parseInt(req.params.page, 10) || 0,
+  var count = parseInt(req.query.count, 10) || 10,
+      page = parseInt(req.query.page, 10) || 0,
       from = page * count,
       to = (page + 1) * count;
+    console.log(count, page, from, to);
   var items = comics.slice(from, to);
   res.json({
     total: comics.length,
